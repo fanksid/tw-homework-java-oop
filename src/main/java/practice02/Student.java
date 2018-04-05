ba9119e9e8687e84ea3706fa15d93f6ef058a198
superxc <superxcgm@gmail.com>
2018-04-05 11:28:02 +0800

    finish pratice 02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,20 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    // 实际意义是class(班级)，但由于class是关键字，所以写成klass
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("I am a Student. I am at Class %d.", klass);
+    }
 }
