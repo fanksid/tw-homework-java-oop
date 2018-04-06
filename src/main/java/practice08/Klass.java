e98ef5f4a968e1de29c2c24e53d9f9d38a62fbb8
superxc <superxcgm@gmail.com>
2018-04-06 18:23:03 +0800

    finish prative08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,37 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        return number == ((Klass)obj).number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", number);
+    }
+
+    public void assignLeader(Student student) {
+        leader = student;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
 }
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,36 @@
 package practice08;
 
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        return id == ((Person)obj).id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.", name, age);
+    }
 }
