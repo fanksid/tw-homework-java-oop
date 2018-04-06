756c52fb19b93c490b43b5544a3d6f94126a82e4
superxc <superxcgm@gmail.com>
2018-04-06 19:08:07 +0800

    finish pratice10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,54 @@
 package practice10;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        return number == ((Klass)obj).number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", number);
+    }
+
+    public void assignLeader(Student student) {
+        if (!isIn(student)) {
+            System.out.println("It is not one of us.");
+            return;
+        }
+        leader = student;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+    }
+
+    @Override
+    public String toString() {
+        return getNumber() + "";
+    }
+
+    public boolean isIn(Student student) {
+        return equals(student.getKlass());
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,67 @@
 package practice10;
 
-public class Teacher {
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Teacher extends Person{
+    private Set<Klass> classes = null;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Set<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    @Override
+    public String introduce() {
+        String sIntroduce = basicIntroduce();
+        if (classes == null || classes.size() == 0) {
+            sIntroduce += " I teach No Class.";
+        } else {
+            // TODO: classes的类型选择是否合适，是否可以通过选取合适的classes类型使得省略下面的排序过程
+            StringBuilder sb = new StringBuilder();
+            Object[] arr = classes.toArray();
+            Arrays.sort(arr, (first, second) -> ((Klass)first).getNumber() - ((Klass)second).getNumber());
+            for (int i = 0; i < arr.length; i++) {
+                if (i > 0) {
+                    sb.append(", ");
+                }
+                sb.append((Klass) arr[i]);
+            }
+            sIntroduce += " I teach Class " + sb.toString() + ".";
+        }
+        return sIntroduce;
+    }
+
+    private String basicIntroduce() {
+        return super.introduce() + " I am a Teacher.";
+    }
+
+
+    public String introduceWith(Student student) {
+        String sIntroduce = basicIntroduce();
+        if (isTeaching(student)) {
+            sIntroduce += String.format(" I teach %s.", student.getName());
+        } else {
+            sIntroduce += String.format(" I don't teach %s.", student.getName());
+        }
+        return sIntroduce;
+    }
+
+    public Set<Klass> getClasses() {
+        return classes;
+    }
+
+    public boolean isTeaching(Student student) {
+        for (Klass klass : classes) {
+            if (klass.isIn(student)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
