06b8e4fa3c86f0eadad784675b438e88fad996da
superxc <superxcgm@gmail.com>
2018-04-06 16:51:29 +0800

    finish pratice06

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,20 @@
 package practice06;
 
-public class Student {
+public class Student extends Person{
+    // 实际意义是class(班级)，但由于class是关键字，所以写成klass
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + String.format(" I am a Student. I am at Class %d.", klass);
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,30 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    // 实际意义是class(班级)，但由于class是关键字，所以写成klass
+    private Integer klass = null;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String sIntroduce = super.introduce() + " I am a Teacher.";
+        if (klass == null) {
+            sIntroduce += " I teach No Class.";
+        } else {
+            sIntroduce += String.format(" I teach Class %d.", klass);
+        }
+        return sIntroduce;
+    }
 }
