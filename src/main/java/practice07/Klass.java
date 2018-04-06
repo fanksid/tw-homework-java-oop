58b98f42fd94b7b6723dd8bdd012723e1da71eb7
superxc <superxcgm@gmail.com>
2018-04-06 17:31:03 +0800

    finish pratice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,28 @@
 package practice07;
 
 public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        return number == ((Klass)obj).number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", number);
+    }
 }
